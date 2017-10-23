841b1b93d996766683aee401a983158056e6673f
Dimitri Marinho <dimitrimarinho@users.noreply.github.com>
2017-10-23 19:02:51 -0300

    New file added - day 23-10-2017

---
+++ Laboratório_de_Programação_I-aula 23-10-2017.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>	// pra usar o vector precisa fazer o include
+using namespace std;
+int main(){
+	int vet[100];
+	vector<int>v;	// vector<int>v; Cria um vetor sem posição alguma , enquanto vector<int>v(100);  Cria um vetor inicial de 100 posições
+	int num;
+	while (cin >> num, num != 0)
+		v.push_back(num); 	// Cria uma nova posição se o a varável num lida com diferente de 0
+	
+
+
+/*  
+
+   for (int i=0; i < v.size();i++)
+	cout << v[i];
+
+    (Forma de funcionamento do vector expressa abaixo)
+
+   for (vector<int>::iteratorit= v.begin();it!=v.end();it++)
+	cout << *it << endl;
+   v.erase(v.begin());			// Apaga o primeiro elemento do vetor 
+   v.erase(v.begin()+2);		// Apaga o terceiro elemento do vetor
+   v.erase(v.begin()+2, v.end());	// Apaga do terceiro elemento do vetor até o elemento final do vetor
+   v.clear();				// Apaga o vetor inteiro. REMOVE O VETOR DA MEMÓRIA. 'FREE' no vetor
+
+
